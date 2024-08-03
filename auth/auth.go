590e526aae47d61f21f7ec334783c73106002932
Nazhif Haidar Putra Wibowo <nzhifhaidar@gmail.com>
2024-08-03 13:28:08 +0700

    Create authentication using JWT

---
+++ auth/auth.go
@@ -0,0 +1,47 @@
+// Package auth
+package auth
+
+import (
+	"errors"
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func GenerateJWT(jwtKey []byte, username string) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &Claims{
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString(jwtKey)
+}
+
+func VerifyJWT(jwtKey []byte, tokenString string) (*Claims, error) {
+	claims := &Claims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	if err != nil {
+		if errors.Is(err, jwt.ErrSignatureInvalid) {
+			return nil, errors.New("invalid token signature")
+		}
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+
+	return claims, nil
+}
--- main.go
+++ main.go
@@ -1,9 +1,79 @@
-package millenium_lms_rest_api_backend
+package main
 
 import (
 	"fmt"
+	"github.com/GNF-Labs/millenium-lms-rest-api-backend/auth"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Tes")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	var jwtKey = []byte(os.Getenv("JWT_SECRET"))
+	if len(jwtKey) == 0 {
+		log.Fatalf("JWT_SECRET is not set in the environment variables")
+	}
+
+	fmt.Println("Environment variables loaded successfully")
+
+	// run the gin router context
+	r := gin.Default()
+	r.POST("/login", func(c *gin.Context) {
+		username := c.PostForm("username")
+		password := c.PostForm("password")
+
+		// Here you would typically check the username and password against your user store
+		if username != "testuser" || password != "testpassword" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid credentials"})
+			return
+		}
+
+		token, err := auth.GenerateJWT(jwtKey, username)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to generate token"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"token": token,
+		})
+	})
+
+	r.GET("/hello", func(c *gin.Context) {
+		bearerToken := c.GetHeader("Authorization")
+		if bearerToken == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Authorization header missing"})
+			return
+		}
+
+		splitToken := strings.Split(bearerToken, " ")
+		if len(splitToken) != 2 {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token format"})
+			return
+		}
+
+		tokenString := splitToken[1]
+		claims, err := auth.VerifyJWT(jwtKey, tokenString)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"username": claims.Username,
+			"token":    claims.RegisteredClaims,
+		})
+	})
+
+	err = r.Run("localhost:8080")
+	if err != nil {
+		log.Fatalf("Failed to start the server: %v", err)
+	}
 }
